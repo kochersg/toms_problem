dc3fb33e0c8433014a15a494417727ca17548616
Gerrit Kocherscheidt <kochersg@koco-group.com>
2024-05-24 14:54:24 +0200

    Analytische Lösung funktioniert

--- src/toms_problem/analytic_solution.py
+++ src/toms_problem/analytic_solution.py
@@ -10,23 +10,62 @@ def calc_selection_probability(problem: problem.Problem, selection_distribution:
         print("Selection invalid: more searched articles in selection then number of searched articles in problem")
         return 0.0
     res = 1.0
-    for sel in selection_distribution:
-        res *= math.comb(problem.n_searched_articles, sel)
+    articles_per_place=distribute_articles_on_places(problem = problem)
+    for ix, sel in enumerate(selection_distribution):
+        res *= math.comb(articles_per_place[ix], sel)
     return res / math.comb(problem.n_articles, problem.n_searched_articles)
 
-def find_combinations(problem: problem.Problem) -> list[list[int]]:
+def distribute_articles_on_places(problem: problem.Problem) -> list[int]:
+    res = [0 for _ in range(problem.n_places)]
+    for ix in range(problem.n_articles):
+        res[ix%problem.n_places]+=1
+    return res
+    
+
+def find_combination_list(problem: problem.Problem) -> list[list[int]]:
     res = []
     ix = 0
     while True:
         r = numberToBase(ix, problem.n_searched_articles+1)
-        print(r)
         if len(r)==problem.n_places:
             if r[0]==problem.n_searched_articles:
+                res.append(r)
                 break
         if sum(r)==problem.n_searched_articles:
             res.append(copy.deepcopy(r))
         ix += 1
     return res
+
+def get_combi_length(combi: list[int]) -> int:
+    l = 0
+    for c in combi:
+        if c != 0:
+            l += 1
+    return l
+
+def sort_combinations_to_number_of_searches(combinations: list[int]):
+    res = {}
+    for c in combinations:
+        combi_len = get_combi_length(combi=c)
+        if combi_len in res.keys():
+            res[combi_len].append(c)
+        else:
+            res[combi_len]=[c]
+    return res
+
+def calc_prob_of_selection_list(problem: problem.Problem, sel_list: list[list[int]]) -> float:
+    res = 0.0
+    for combi in sel_list:
+        res+=calc_selection_probability(problem=problem, selection_distribution=combi)
+    return res
+
+
+def calc_probability_distribution(problem: problem.Problem, combi_dict: dict[int, list[list[int]]]) -> list[int, int]:
+    res = []
+    for c in combi_dict:
+        res.append([c, calc_prob_of_selection_list(problem=problem, sel_list = combi_dict[c])])
+    return res
+
 
 def numberToBase(n, b):
     if n == 0:
@@ -38,9 +77,11 @@ def numberToBase(n, b):
     return digits[::-1]
 
 def math_test():
-    p = problem.Problem(n_articles=21, n_places=3, n_searched_articles=5)
-    print(calc_selection_probability(problem=p, selection_distribution=[3,1,1]))
-    print(find_combinations(problem=p))
+    p = problem.Problem(n_articles=10000, n_places=5, n_searched_articles=10)
+    print(c:=find_combination_list(problem=p))
+    print(d:=sort_combinations_to_number_of_searches(combinations=c))
+    print(r:=calc_probability_distribution(problem=p, combi_dict=d))
+    print(sum([c[1] for c in r]))
 
 
 
